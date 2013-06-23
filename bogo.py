d7d2b77ae0aa0510c395cd18b3e115a355088e2f
Trung Ngo <ndtrung4419@gmail.com>
2013-06-23 20:42:11 +0700

    Refactoring and comments; Add a rule for "ư"

--- bogo.py
+++ bogo.py
@@ -13,6 +13,7 @@ im = {
             {"result": "ê", "base": "e", "mod": "e"},
             {"result": "ô", "base": "o", "mod": "o"},
             {"result": "ơ", "base": "o", "mod": "w"},
+            {"result": "ư", "base": "u", "mod": "w"},
             {"result": "đ", "base": "d", "mod": "d"}
         ],
     "tone_addition": [],
@@ -20,6 +21,20 @@ im = {
 }
 
 im["tone_addition"] = rules.gen_tone_addition_rules("fsrxj")
+
+# A typical re_list (actually, a dict) looks like this:
+# {
+#     "character_creation": [
+#         [r"^a([^a]*)a(.*)", "â"],
+#         [r"^uo([^w]*)w(.*)", "ươ"]
+#     ],
+#     "tone_addition": [
+#         [r'^ê([^r]*)r(.*)', 'ể'],
+#     ]
+# }
+#
+# For performance reasons, the regular expression string part is pre-compiled
+# into an object beforehand.
 re_list = rules.gen_re_list(im)
 
 
@@ -31,17 +46,22 @@ def process_word(word):
         except:
             return True
 
-    # Process the raw input string in "phrases", each time iterating over the
+    # Process the raw input string in phrases, each time iterating over the
     # whole string with a different set of rules.
-    for rule_group in ["character_creation", "tone_addition"]:
-        rule_group = re_list[rule_group]
+    for rule_set in ["character_creation", "tone_addition"]:
+        rule_set = re_list[rule_set]
         k = 0
         while not is_out_of_range(word, k):
             head, tail = word[:k], word[k:]
-            for exp in rule_group:
-                new_string = exp[0].sub(exp[1] + r"\1\2", tail)
+            for exp in rule_set:
+                regexp_object, replace_with = exp[0], exp[1]
+                new_string = regexp_object.sub(replace_with + r"\1\2", tail)
                 if new_string != tail:
                     tail = new_string
+                    # Decreasing k by one here is offset by k += 1 down below,
+                    # effectively keeps the cursor from moving and reprocess
+                    # the current character one more time. This is to allow
+                    # undoing. ie: process_word("aaa") -> "âa" -> "a"
                     k -= 1
                     break
 
@@ -49,6 +69,3 @@ def process_word(word):
             k += 1
 
     return word
-
-
-
--- test/test_re_tools.py
+++ test/test_re_tools.py
@@ -1,22 +1,26 @@
 from nose.tools import eq_
-from bogo import gen_re, gen_re_list
+from rules import gen_re, gen_re_list
+import re
 
 
 def test_gen_re():
-    eq_(gen_re({"base": "a", "mod": "s"}), r"^a([^s]*)s(.*)")
-    eq_(gen_re({"base": "uo", "mod": "w"}), r"^uo([^w]*)w(.*)")
+    eq_(gen_re({"base": "a", "mod": "s"}), re.compile(r"^a([^s]*)s(.*)"))
+    eq_(gen_re({"base": "uo", "mod": "w"}), re.compile(r"^uo([^w]*)w(.*)"))
 
 
 def test_gen_re_list():
-	im = [
+	im = {
+		"tone_addition": [
 	    {"result": "á", "base": "a", "mod": "s"},
 	    {"result": "ươ", "base": "uo", "mod": "w"}
-	]
+	]}
 
-	good_re_list = [
-	    [r"^a([^s]*)s(.*)", "á"],
-	    [r"^uo([^w]*)w(.*)", "ươ"]
-	]
+	good_re_list = {
+		"tone_addition": [
+	    [re.compile(r"^a([^s]*)s(.*)"), "á"],
+	    [re.compile(r"^uo([^w]*)w(.*)"), "ươ"]
+	]}
+
 
 	re_list = gen_re_list(im)
 	eq_(re_list, good_re_list)
