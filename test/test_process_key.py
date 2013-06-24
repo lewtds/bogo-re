84a3a66db4a13ee30a0b64bb318aba5659182051
Trung Ngo <ndtrung4419@gmail.com>
2013-06-24 12:29:04 +0700

    Add tests that failed in benchmark.py to test/test_process_key.py

--- test/test_process_key.py
+++ test/test_process_key.py
@@ -17,3 +17,41 @@ def test_single_vowel_words():
 def test_complex_closed_vowels():
     eq_(process_word("thuownrg"), "thưởng")
     eq_(process_word("chuyener"), "chuyển")
+
+
+def test_failed_tests():
+    test_cases = [
+        ('biesue', 'biếu'),
+        ('bieseu', 'biếu'),
+        ('bieesu', 'biếu'),
+        ('biereu', 'biểu'),
+        ('bieeru', 'biểu'),
+        ('bierue', 'biểu'),
+        ('booong', 'boong'),
+        ('booosng', 'boóng'),
+        ('booonsg', 'boóng'),
+        ('booongs', 'boóng'),
+        ('buofoi', 'buồi'),
+        ('buofio', 'buồi'),
+        ('buoofi', 'buồi'),
+        ('buoori', 'buổi'),
+        ('buorio', 'buổi'),
+        ('buoroi', 'buổi'),
+        ('buosuw', 'bướu'),
+        ('buwosuw', 'bướu'),
+        ('buowsu', 'bướu'),
+        ('buwowsu', 'bướu'),
+        ('buwoswu', 'bướu'),
+        ('buoswu', 'bướu'),
+        ('buwowri', 'bưởi'),
+        ('buowri', 'bưởi'),
+        ('buworiw', 'bưởi'),
+        ('buworwi', 'bưởi'),
+        ('buorwi', 'bưởi'),
+        ('buoriw', 'bưởi')]
+
+    def atomic(input_, expected):
+        eq_(process_word(input_), expected)
+
+    for case in test_cases:
+        yield atomic, case[0], case[1]
