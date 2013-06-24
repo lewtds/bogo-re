41f92534464caa716e0c1586b6371baf3b7eb017
Trung Ngo <ndtrung4419@gmail.com>
2013-06-24 12:28:16 +0700

    Performance tweak in bogo.py - 10 times faster now :dancer:

--- bogo.py
+++ bogo.py
@@ -22,19 +22,18 @@ im = {
 
 im["tone_addition"] = rules.gen_tone_addition_rules("fsrxj")
 
-# A typical re_list (actually, a dict) looks like this:
+# A typical im after gen_re_list() looks just like the original one, but with
+# each of its rules getting a new "regexp" key:
+#
 # {
-#     "character_creation": [
-#         [r"^a([^a]*)a(.*)", "â"],
-#         [r"^uo([^w]*)w(.*)", "ươ"]
-#     ],
+#     "character_creation": [...],
 #     "tone_addition": [
-#         [r'^ê([^r]*)r(.*)', 'ể'],
-#     ]
+#         {"result": "á", "base": "a", "mod": "s", "regexp": re.compile(r"^a([^s]*)s(.*)")},
+#         {"result": "ươ", "base": "uo", "mod": "w", "regexp": re.compile(r"^uo([^w]*)w(.*)")}
+#     ],
+#     ...
 # }
 #
-# For performance reasons, the regular expression string part is pre-compiled
-# into an object beforehand.
 re_list = rules.gen_re_list(im)
 
 
@@ -46,6 +45,8 @@ def process_word(word):
         except:
             return True
 
+
+
     # Process the raw input string in phrases, each time iterating over the
     # whole string with a different set of rules.
     for rule_set in ["character_creation", "tone_addition"]:
@@ -53,19 +54,22 @@ def process_word(word):
         k = 0
         while not is_out_of_range(word, k):
             head, tail = word[:k], word[k:]
-            for exp in rule_set:
-                regexp_object, replace_with = exp[0], exp[1]
-                new_string = regexp_object.sub(replace_with + r"\1\2", tail)
-                if new_string != tail:
-                    tail = new_string
-                    # Decreasing k by one here is offset by k += 1 down below,
-                    # effectively keeps the cursor from moving and reprocess
-                    # the current character one more time. This is to allow
-                    # undoing. ie: process_word("aaa") -> "âa" -> "a"
-                    k -= 1
-                    break
+            for rule in rule_set:
+                # This line makes it FASSSTEEERRRR!!!
+                if word.find(rule["base"]) != -1:
+                    new_string = rule["regexp"].sub(rule["result"] + r"\1\2", tail)
+                    if new_string != tail:
+                        tail = new_string
+                        # Decreasing k by one here is offset by k += 1 down below,
+                        # effectively keeps the cursor from moving and reprocess
+                        # the current character one more time. This is to allow
+                        # undoing. ie: process_word("aaa") -> "âa" -> "a"
+                        k -= 1
+                        break
 
             word = head + tail
             k += 1
 
     return word
+
+print(process_word("chuyênr"))
